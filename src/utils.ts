9e79abba8b535cfdb2bc148d1063887fab47b2aa
Junyoung Choi <rokt33r.choi@gmail.com>
2020-04-12 17:34:44 +0900

    Fix utils
    - Add createTypeValidator
    - Remove appendKeyToValidationResult

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,17 @@
 import is from '@sindresorhus/is'
 import { Validator } from './tval'
 
-export function appendKeyToValidationResult(result: string, key: string) {
-  return result
-    .replace(/^Expected value/, `Expected property, \`${key}\`,`)
-    .replace(/^Expected property, `(.+)`/, `Expected property, \`$1.${key}\``)
-}
-
 export function createTypeValidator<T>(expectedType: string): Validator<T> {
   return value => {
     const valueType = is(value)
     if (valueType === expectedType) {
       return null
     }
-    return `Expected value to be of type \`${expectedType}\` but received type \`${valueType}\``
+    return {
+      code: 'type',
+      value,
+      messagePredicate: `be of type \`${expectedType}\` but received type \`${valueType}\``,
+      validatorArgs: [expectedType]
+    }
   }
 }
