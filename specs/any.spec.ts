701e0f047df9e1ccd1b2956142dbd6e48696c9b4
Junyoung Choi <rokt33r.choi@gmail.com>
2020-04-12 18:21:07 +0900

    Fix any test

--- specs/any.spec.ts
+++ specs/any.spec.ts
@@ -1,27 +1,29 @@
-import { tAny, report, tStr, tNum } from '../src'
+import { tAny, validate, tStr, tNum } from '../src'
 
 describe('tAny', () => {
   it('validates with multiple conditions(1st)', () => {
-    const result = report(tAny(tStr(), tNum()), 123)
+    const result = validate(tAny(tStr(), tNum()), 123)
 
     expect(result).toBe(null)
   })
 
   it('validates with multiple conditions(2nd)', () => {
-    const result = report(tAny(tStr(), tNum()), 'string')
+    const result = validate(tAny(tStr(), tNum()), 'string')
 
     expect(result).toBe(null)
   })
 
   it('reports a surmarized error message if value does not match any of conditions', () => {
-    const result = report(tAny(tStr(), tNum()), {})
+    const result = validate(tAny(tStr(), tNum()), {})
 
-    expect(result).toBe(
-      [
-        'Expected value to match any of following conditions',
-        '- Expected value to be of type `string` but received type `Object`',
-        '- Expected value to be of type `number` but received type `Object`'
-      ].join('\n')
-    )
+    expect(result).toMatchObject({
+      code: 'any',
+      subResults: [
+        {
+          code: 'type'
+        },
+        { code: 'type' }
+      ]
+    })
   })
 })
