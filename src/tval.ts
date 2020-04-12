79b637acb45b6a89045766c014b8f51886f80696
Junyoung Choi <rokt33r.choi@gmail.com>
2020-04-12 17:33:03 +0900

    Introduce InvalidResult

--- src/tval.ts
+++ src/tval.ts
@@ -1,5 +1,54 @@
+export interface InvalidResult {
+  valueType?: string
+  valuePaths?: string[]
+  code: string
+  value: any
+  messagePredicate: string
+  subResults?: InvalidResult[]
+  validatorArgs?: any[]
+}
+
+export class ValidationError extends Error {
+  code: string
+  value: any
+  subResults?: InvalidResult[]
+  validatorArgs?: any[]
+
+  constructor(invalidResult: InvalidResult, context: Function) {
+    const message = getInvalidErrorMessage(invalidResult)
+    super(message)
+
+    /* istanbul ignore next */
+    if ('captureStackTrace' in Error) {
+      Error.captureStackTrace(this, context)
+    }
+
+    this.name = 'ArgumentError'
+    this.code = invalidResult.code
+    this.value = invalidResult.value
+    this.validatorArgs = invalidResult.validatorArgs
+  }
+}
+
+export function isValidationError(error: Error): error is ValidationError {
+  return error instanceof ValidationError
+}
+
+export function getInvalidErrorMessage({
+  valueType,
+  valuePaths,
+  messagePredicate
+}: InvalidResult): string {
+  const prefix = valueType != null ? `${valueType} ` : ''
+  const subject =
+    valuePaths != null && valuePaths.length > 0
+      ? `The ${prefix}property, ${valuePaths.join('.')}`
+      : `The ${prefix}value`
+  return `${subject} should ${messagePredicate}`
+}
+
 export interface Validator<T, C = {}> {
-  (input: any, context?: C): string | null
+  (input: any, context?: C): InvalidResult | null
 }
 
 export interface Predicate<R> {
@@ -24,35 +73,10 @@ export type Unshape<S> = S extends ShapeSchema
     }
   : never
 
-export function assert<R>(
+export function validate<R>(
   predicate: Predicate<R>,
   value: any
-): asserts value is R {
-  const context = {}
-  const { validators } = predicate
-  for (const validator of validators) {
-    const result = validator(value, context)
-    if (result == null) {
-      continue
-    }
-    throw new Error(result)
-  }
-}
-
-export function isValid<R>(predicate: Predicate<R>, value: any): value is R {
-  const context = {}
-  const { validators } = predicate
-  for (const validator of validators) {
-    const result = validator(value, context)
-    if (result == null) {
-      continue
-    }
-    return false
-  }
-  return true
-}
-
-export function report<R>(predicate: Predicate<R>, value: any): string | null {
+): InvalidResult | null {
   const context = {}
   const { validators } = predicate
   for (const validator of validators) {
@@ -63,4 +87,19 @@ export function report<R>(predicate: Predicate<R>, value: any): string | null {
     return result
   }
   return null
+}
+
+export function assert<R>(
+  predicate: Predicate<R>,
+  value: any
+): asserts value is R {
+  const result = validate(predicate, value)
+  if (result != null) {
+    throw new ValidationError(result, assert)
+  }
+}
+
+export function isValid<R>(predicate: Predicate<R>, value: any): value is R {
+  const result = validate(predicate, value)
+  return result == null
 }
